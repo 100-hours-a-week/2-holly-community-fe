de4ed4b32356a57f995fb4cb2f8cf3b7dc2d3b89
holly.kim <126642836+mellonmusk@users.noreply.github.com>
2025-03-20 11:01:36 +0900

    비밀번호 암호화 적용

--- js/api/request.js
+++ js/api/request.js
@@ -315,14 +315,93 @@ export async function deleteUser(userId) {
   }
 }
 
-export async function loginUser(email, password) {
-  const response = await fetch(`${BASE_URL}/users`);
-  const users = await response.json();
+// export async function loginUser(email, password) {
+//   const response = await fetch(`${BASE_URL}/users`);
+//   const users = await response.json();
 
-  const user = users.find((u) => u.email === email && u.password === password);
-  console.log(user);
-  return user || null; // 로그인 성공 시 사용자 객체 반환, 실패 시 null 반환
+//   const user = users.find((u) => u.email === email && u.password === password);
+//   console.log(user);
+//   return user || null; // 로그인 성공 시 사용자 객체 반환, 실패 시 null 반환
+// }
+export async function loginUser(email, password) {
+  const response = await fetch(`${BASE_URL}/users/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    console.error('로그인 실패:', response.statusText);
+    return null;
+  }
+  const user = response.json(); 
+  return user;
 }
+/*
+export async function setToken(email, password) {
+  const response = await fetch(`${BASE_URL}/users/login`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    console.error('로그인 실패:', response.statusText);
+    return null;
+  }
+  const token = await response.text();
+  console.log(token); 
+  if (response) {
+    localStorage.setItem('jwt', token); // JWT 토큰을 로컬 스토리지에 저장
+  } else {
+    console.error('토큰이 응답에 없습니다.');
+    return null;
+  }
+
+  return token;
+}
+
+
+export async function authFetch(url, options = {}) { 
+
+  if (!token) {
+    console.error('인증 토큰이 없습니다. 로그인하세요.');
+    return null;
+  }
+
+  const headers = {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`, // JWT를 Authorization 헤더에 추가
+  };
+
+  const response = await fetch(url, {
+    ...options,
+    headers: { ...headers, ...options.headers }, // 기존 headers와 병합
+  });
+
+  if (response.status === 401) {
+    console.error('인증 실패: 토큰이 유효하지 않음');
+    return null;
+  }
+
+  return response.json();
+}
+
+
+export async function getUser() {
+  return authFetch(`${BASE_URL}/users/me`);
+}
+
+export function logoutUser() {
+  localStorage.removeItem('jwt'); // JWT 토큰 삭제
+  console.log('로그아웃되었습니다.');
+}
+
+*/
 
 // 댓글 목록 가져오기 (GET)
 export async function getComments(post) {
